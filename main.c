03a3ae511b7c351fa56cb5e02254ee91514fb1fa
GozikizoG <cypriencosta@gmail.com>
2025-09-21 18:24:52 +0200

    Verif+clean des conditions inutiles

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
+#include <math.h>
 #include <stdio.h> // pour les input et output (littéralement obligatoire en C)
 #include <stdlib.h> // pour le random
 #include <time.h>  // pour avoir une valeur impredictible pour la seed du random
 
 int main() {
     int size;
+    int c;
     int grid[35][35] = {};
     srand(time(NULL));
 
-    printf("Quel est la taille de la grille ? (MAX 35)"); // dire max 100
-    scanf("%d", &size); //verif plus tard
+
+    do{printf("Quel est la taille de la grille [5-35] : ");
+
+        if (scanf("%d", &size) != 1) {
+            printf("Erreur, mettez un nombre entier.\n");
+            while ((c = getchar()) != '\n' && c != EOF);
+            size=0;
+            continue;
+        }
+
+        printf("Taille de la grille = %d \n ", size);
+        printf("La taille est invalide, cela doit etre compris entre 5 et 35. \n\n");
+
+    }while (size>35 || size<5);
+
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -16,15 +31,14 @@ int main() {
         }
     }
 
-    printf("    ");
+    printf("   ");
     for (int i = 0; i < size; i++){
-        if (i+1<9){printf("%d  ",i+1);}else{printf("%d ",i+1);}
+        printf("%2d ",i+1);
     }
         printf("\n");
     for (int i = 0; i < size; i++) {
 
         printf("%2d ",i+1);
-        //if (i<=10){printf(" ");}
 
         for (int j = 0; j < size; j++) {
 
